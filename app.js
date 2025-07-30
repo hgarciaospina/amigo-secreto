640a2b557524de9f210c326cc16a69377fb8a0bf
Henry García <henrygarciaospina@gmail.com>
2025-07-30 00:09:44 -0500

    Se implementa funión para capitalizar los nombres.

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ let listaAmigos = [];
 
 function agregarAmigo() {
   const input = document.getElementById("amigo");
-  const nombreDelAmigo = input.value.trim();
-  console.log(nombreDelAmigo);
+  let nombreDelAmigo = input.value.trim();
   if (esNombreValido(nombreDelAmigo)) {
+    nombreDelAmigo = capitalizarNombre(nombreDelAmigo);
     listaAmigos.push(nombreDelAmigo);
   } else {
     alert(
@@ -19,4 +19,14 @@ function agregarAmigo() {
 function esNombreValido(nombre) {
   const regex = /^([A-Za-zÁÉÍÓÚáéíóúÑñ]{2,})( [A-Za-zÁÉÍÓÚáéíóúÑñ]{2,})*$/;
   return regex.test(nombre.trim());
+}
+
+function capitalizarNombre(texto) {
+  return texto
+    .trim()
+    .toLowerCase()
+    .split(" ")
+    .filter((p) => p.length > 0)
+    .map((p) => p[0].toUpperCase() + p.slice(1))
+    .join(" ");
 }
