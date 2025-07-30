cc43a50cf18679186de2908730e268c82d5c1942
Henry García <henrygarciaospina@gmail.com>
2025-07-29 23:30:51 -0500

    Se implementa función para validar el nombre.

--- app.js
+++ app.js
@@ -3,8 +3,20 @@ let listaAmigos = [];
 function agregarAmigo() {
   const input = document.getElementById("amigo");
   const nombreDelAmigo = input.value.trim();
-  listaAmigos.push(nombreDelAmigo);
+  console.log(nombreDelAmigo);
+  if (esNombreValido(nombreDelAmigo)) {
+    listaAmigos.push(nombreDelAmigo);
+  } else {
+    alert(
+      "Nombre inválido. Usa solo letras (con tildes y ñ), mínimo 2 por palabra."
+    );
+  }
   input.value = "";
   input.focus();
-  console.log(nombreDelAmigo);
+  return;
+}
+
+function esNombreValido(nombre) {
+  const regex = /^([A-Za-zÁÉÍÓÚáéíóúÑñ]{2,})( [A-Za-zÁÉÍÓÚáéíóúÑñ]{2,})*$/;
+  return regex.test(nombre.trim());
 }
