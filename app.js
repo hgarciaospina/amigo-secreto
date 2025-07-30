9332490ff03e4189eea56f5f256e987793bbc929
Henry García <henrygarciaospina@gmail.com>
2025-07-30 00:43:01 -0500

    Implementación de función para limpiar el input.

--- app.js
+++ app.js
@@ -2,21 +2,52 @@ let listaAmigos = [];
 
 function agregarAmigo() {
   const input = document.getElementById("amigo");
-  const nombreDelAmigo = input.value.trim();
-  console.log(nombreDelAmigo);
+  let nombreDelAmigo = input.value.trim();
+
+  // Validación de duplicado
+  if (existeNombre(nombreDelAmigo)) {
+    let mostrarMensajeError = `El nombre "${capitalizarNombre(
+      nombreDelAmigo
+    )}" ya fue ingresado.`;
+
+    alert(mostrarMensajeError);
+    limpiarInput(input);
+  }
+
   if (esNombreValido(nombreDelAmigo)) {
+    nombreDelAmigo = capitalizarNombre(nombreDelAmigo);
     listaAmigos.push(nombreDelAmigo);
   } else {
     alert(
       "Nombre inválido. Usa solo letras (con tildes y ñ), mínimo 2 por palabra."
     );
   }
-  input.value = "";
-  input.focus();
-  return;
+  limpiarInput(input);
+}
+
+function existeNombre(nombre) {
+  return listaAmigos.some(
+    (amigo) => amigo.toLowerCase() === nombre.toLowerCase()
+  );
 }
 
 function esNombreValido(nombre) {
   const regex = /^([A-Za-zÁÉÍÓÚáéíóúÑñ]{2,})( [A-Za-zÁÉÍÓÚáéíóúÑñ]{2,})*$/;
   return regex.test(nombre.trim());
+}
+
+function capitalizarNombre(texto) {
+  return texto
+    .trim()
+    .toLowerCase()
+    .split(" ")
+    .filter((p) => p.length > 0)
+    .map((p) => p[0].toUpperCase() + p.slice(1))
+    .join(" ");
+}
+
+function limpiarInput(entrada) {
+  entrada.value = "";
+  entrada.focus();
+  return;
 }
